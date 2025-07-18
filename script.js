bbd002b728681a40e49e8998be0e158127a70cde
Derffson <derffson7@gmail.com>
2025-07-18 12:03:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -218,7 +218,7 @@ async function gerarEventos() {
   const eventosAvisos = avisos
     .filter(a => a.data)
     .map(a => ({
-      title: "📢 Aviso",
+      title: "📢",
       start: a.data,
       backgroundColor: "#FFA500",
       borderColor: "#FF8C00",
